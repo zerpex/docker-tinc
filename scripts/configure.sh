51aefbe59460072b29538242ea23fd8a557c0a2e
Guillermo Bisheimer <gbisheimer@bys-control.com.ar>
2016-11-13 11:22:06 -0300

    Update configure.sh script

--- scripts/configure.sh
+++ scripts/configure.sh
@@ -70,7 +70,7 @@ if [[ $TINC_WEIGHT ]]; then tinc set Weight $TINC_WEIGHT; fi
 
 # Create up & down scripts
 echo "#!/bin/sh
-ifconfig \$INTERFACE $TINC_SUBNET netmask $TINC_NETMASK" > /etc/tinc/tinc-up
+ifconfig \$INTERFACE $TINC_IP netmask $TINC_NETMASK" > /etc/tinc/tinc-up
 
 echo '#!/bin/sh
 ifconfig $INTERFACE down' > /etc/tinc/tinc-down
